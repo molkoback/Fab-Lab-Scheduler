6e83a3c4d315cb7978683a076618b5565e8896a4
molko <molkoback@gmail.com>
2018-11-11 20:00:02 +0200

    Basic HTTP authentication

--- application/config/tokens.php
+++ application/config/tokens.php
@@ -1,3 +1,5 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
 
 $config['tokens']['token_api_url'] = "http://localhost:8080/api/";
+$config['tokens']['token_api_user'] = "scheduler";
+$config['tokens']['token_api_passwd'] = "password";
--- application/libraries/Tokens.php
+++ application/libraries/Tokens.php
@@ -4,6 +4,7 @@ class Tokens {
 	protected $CI;
 	protected $config_vars;
 	protected $url = '';
+	protected $userpwd = '';
 	
 	public function __construct() {
 		$this->CI = & get_instance();
@@ -12,6 +13,9 @@ class Tokens {
 		$this->CI->config->load('tokens');
 		$this->config_vars = $this->CI->config->item('tokens');
 		$this->url = $this->config_vars['token_api_url'];
+		$user = $this->config_vars['token_api_user'];
+		$pwd = $this->config_vars['token_api_passwd'];
+		$this->userpwd = "$user:$pwd";
 	}
 	
 	private function parse_data($data) {
@@ -29,6 +33,8 @@ class Tokens {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL,"{$this->url}?token_id=$id");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_USERPWD, $this->userpwd);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 		$data = curl_exec($ch);
 		curl_close($ch);
 		return $this->parse_data($data);
@@ -40,6 +46,8 @@ class Tokens {
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, "token_id=$id&tokens=$tokens");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_USERPWD, $this->userpwd);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 		$data = curl_exec($ch);
 		curl_close($ch);
 		return $this->parse_data($data);
